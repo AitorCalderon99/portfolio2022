6bbbe492e05ab748ebf884e1dad699f8a034eff0
AitorCalderonLo <aitor.calderonlo@elorrieta-errekamari.com>
2022-10-06 21:15:52 +0200

    Colorful rain background and starting header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BackgroundRainDirective } from './shared/background-rain.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BackgroundRainDirective
   ],
   imports: [
     BrowserModule
