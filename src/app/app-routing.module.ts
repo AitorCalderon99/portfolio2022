6ede17e62cffc376e415c07c1f8ff1ea07b42b64
AitorCalderonLo <aitor.calderonlo@elorrieta-errekamari.com>
2022-10-12 17:59:07 +0200

    Adding angular routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {WorkComponent} from "./work/work.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BackgroundRainDirective } from './shared/bacground-rain/background-rain.directive';
 import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BackgroundRainDirective,
-    HomeComponent
+    HomeComponent,
+    WorkComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
